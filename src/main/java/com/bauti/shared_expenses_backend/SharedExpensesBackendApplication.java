c7803cdfab6d085ebd0f6fc2ad95ec478feb69c4
Bauti Corti <bautistamariacorti@gmail.com>
2026-05-04 13:04:54 -0300

    springboot set up

---
+++ src/main/java/com/bauti/shared_expenses_backend/SharedExpensesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bauti.shared_expenses_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedExpensesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedExpensesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bauti/shared_expenses_backend/SharedExpensesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bauti.shared_expenses_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedExpensesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
